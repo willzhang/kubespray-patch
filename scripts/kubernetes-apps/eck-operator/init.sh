39f83e87473785d90d0c3da59cd9bf59e0575545
will <willzhmic@outlook.com>
2023-02-04 16:04:43 +0800

    Update init.sh

--- scripts/kubernetes-apps/eck-operator/init.sh
+++ scripts/kubernetes-apps/eck-operator/init.sh
@@ -4,10 +4,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 export readonly NAME=${1:-$(basename "$PWD")}
 export readonly VERSION=${2:-$(basename "$PWD")}
 
+VERSION=${VERSION#v}
 mkdir -p images/shim
-cat <<EOF >"images/shim/${NAME}-images.txt"
-docker.elastic.co/eck/eck-operator:${VERSION#v}
-docker.elastic.co/elasticsearch/elasticsearch:8.3.1
-docker.elastic.co/beats/filebeat:8.3.1
-docker.elastic.co/kibana/kibana:8.3.1
+wget -qO- https://github.com/elastic/cloud-on-k8s/archive/refs/tags/${VERSION}.tar.gz | tar -xz
+components_version=$(helm show values cloud-on-k8s-${VERSION}/deploy/eck-beats --jsonpath {.version})
+cat <<EOF> "images/shim/${NAME}-images.txt"
+docker.elastic.co/eck/eck-operator:${VERSION}
+docker.elastic.co/elasticsearch/elasticsearch:${components_version}
+docker.elastic.co/beats/filebeat:${components_version}
+docker.elastic.co/kibana/kibana:${components_version}
 EOF
+
+mkdir -p files/shim
+echo "https://helm.elastic.co/helm/eck-operator/eck-operator-${VERSION}.tgz" > files/shim/${NAME}-files.txt
