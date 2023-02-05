b006d60f9aade6f03f267b7ddc738f3719244994
will <willzhmic@outlook.com>
2023-02-06 01:32:10 +0800

    Update init.sh

--- scripts/kubernetes-apps/init.sh
+++ scripts/kubernetes-apps/init.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 while read name version
 do
   bash ${name}/init.sh $name ${version}
-  [ ! -z "$(ls -A ${name}/images/shim 2>/dev/null)" ] && cat ${name}/images/shim/*-images.txt >> ./kubernetes_apps-images.txt
-  [ ! -z "$(ls -A ${name}/files/shim 2>/dev/null)" ] && cat ${name}/files/shim/*-files.txt >> ./kubernetes_apps-files.txt
 done < versions.txt
+
+find ./ -name "*-images.txt" | xargs cat | sort -u > ./kubernetes_apps-images.list
+find ./ -name "*-files.txt" | xargs cat | sort -u > ./kubernetes_apps-files.list
