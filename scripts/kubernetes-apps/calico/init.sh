e80451c3631807a4d506843007bbf1597bdb86a2
will <willzhmic@outlook.com>
2023-02-08 00:47:19 +0800

    Update init.sh

--- scripts/kubernetes-apps/calico/init.sh
+++ scripts/kubernetes-apps/calico/init.sh
@@ -9,3 +9,6 @@ mkdir -p images/shim
 cat tigera-operator.yaml |grep quay.io | awk -F" " '{print $2}' > images/shim/${NAME}-images.txt
 echo "docker.io/calico/csi:${VERSION}" >> images/shim/${NAME}-images.txt
 echo "docker.io/calico/node-driver-registrar:${VERSION}" >> images/shim/${NAME}-images.txt
+
+mkdir -p files/shim
+echo "https://github.com/projectcalico/calico/releases/download/${VERSION}/tigera-operator-${VERSION}.tgz" > files/shim/${NAME}-files.txt
