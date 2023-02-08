915d4da09bcf7823bf85d87309a047a1f11b7e4c
willzhang <willzhmic@outlook.com>
2023-02-09 01:59:47 +0800

    add ingress-nginx

---
+++ scripts/kubernetes-apps/ingress-nginx/init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+export readonly NAME=${1:-$(basename "$PWD")}
+export readonly VERSION=${2:-$(basename "$PWD")}
+
+VERSION=${VERSION#v}
+wget -q https://github.com/kubernetes/ingress-nginx/releases/download/helm-chart-${VERSION}/ingress-nginx-${VERSION}.tgz
+
+mkdir -p images/shim
+helm template ingress-nginx-${VERSION}.tgz \
+--set controller.image.digest=null \
+--set controller.admissionWebhooks.patch.image.digest=null | grep image: | sort -u  | awk -F" " '{print $2}' | tr -d '"' > images/shim/ingress-nginx-files.txt
+
+mkdir -p  files/shim
+echo "https://github.com/kubernetes/ingress-nginx/releases/download/helm-chart-${VERSION}/ingress-nginx-${VERSION}.tgz" > files/shim/ingress-nginx-files.txt
